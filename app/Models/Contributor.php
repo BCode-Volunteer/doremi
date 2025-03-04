37c5250e046c060866515762d757b7091acfd0d4
Daniel <drp6@aluno.ifnmg.edu.br>
2025-03-04 12:21:00 -0300

    [DOREMI-14][Backend]: [FEAT] Relacionamentos entre Contribuição e Contribuidor

--- app/Models/Contributor.php
+++ app/Models/Contributor.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contributor extends Model
 {
     use HasFactory;
 
     protected $table = 'contributors';
-
     protected $fillable = ['name', 'email', 'phone'];
+
+    public function contributions(): HasMany
+    {
+        return $this->hasMany(Contribution::class);
+    }
 }
