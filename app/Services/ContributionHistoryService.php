03c268ab7b34df50b51af15bd4094f5f3f9a072d
Daniel <drp6@aluno.ifnmg.edu.br>
2025-03-04 13:04:45 -0300

    [DOREMI-14][Backend]: [FEAT] Service ContributionHistory

---
+++ app/Services/ContributionHistoryService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\ContributionException;
+use App\Models\Contribution;
+use App\Models\ContributionHistory;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+
+class ContributionHistoryService implements IContributionHistoryService
+{
+    public function listContributionHistory(): Collection
+    {
+        return ContributionHistory::all();
+    }
+
+    public function createContributionHistory(
+        Contribution $contribution,
+        float $amount,
+        Carbon $date = null
+    ): ContributionHistory {
+        try {
+            DB::beginTransaction();
+
+            if (is_null($date)) {
+                $date = Carbon::now();
+            }
+
+            $history = ContributionHistory::create([
+                'contribution_id' => $contribution->id,
+                'amount' => $amount,
+                'date' => $date,
+            ]);
+
+            DB::commit();
+
+            return $history;
+        } catch (\Exception) {
+            DB::rollBack();
+            throw new ContributionException(
+                'Erro interno ao cadastrar histórico de contribuição! Tente novamente mais tarde.'
+            );
+        }
+    }
+}
